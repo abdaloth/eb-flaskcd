c1f765ca5df4b991e015bd3f76eae39b2f11ebe9
abdaloth <abdullahoalothman@gmail.com>
2021-02-08 18:30:58 -0500

    stylized the landing page

--- application.py
+++ application.py
@@ -1,21 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    """basic landing page."""
-    return """<h1> Elastic Beanstalk CD</h1>"""
-
-
-@application.route('/html')
-def html():
-    """Returns some custom HTML"""
-    return """
-    <title>This is a Hello World World Page</title>
-    <p>Hello</p>
-    <p><b>World</b></p>
-    """
+    """simple landing Page."""
+    return render_template('hello_eb.html')
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port=8080, debug=True)
